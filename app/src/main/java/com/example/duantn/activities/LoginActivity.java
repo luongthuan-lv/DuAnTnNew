704584280477f98359db332845a38cbf460585b3
DucAnh <nguyenducanh12042000@gmail.com>
2020-10-23 14:20:11 +0700

    giao dien man Login, sua man chon ngon ngu

--- app/src/main/java/com/example/duantn/activities/LoginActivity.java
+++ app/src/main/java/com/example/duantn/activities/LoginActivity.java
@@ -1,9 +1,8 @@
 package com.example.duantn.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.duantn.R;
 
@@ -13,10 +12,24 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        findViewById(R.id.btn_google).setOnClickListener(this);
+        findViewById(R.id.btn_facebook).setOnClickListener(this);
+
+
     }
+
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()) {
+            case R.id.btn_google:
+                showToast("Google");
+                nextActivity(SelectLanguageActivity.class);
+                break;
+            case R.id.btn_facebook:
+                showToast("Facebook");
+                nextActivity(SelectLanguageActivity.class);
+                break;
+        }
     }
 }
