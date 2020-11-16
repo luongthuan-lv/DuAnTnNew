cd570982e0ac8f1eb07eb8b405a87df0afc3130e
DucAnh <nguyenducanh12042000@gmail.com>
2020-11-16 11:04:36 +0700

    a

--- app/src/main/java/com/example/duantn/activities/TourListActivity.java
+++ app/src/main/java/com/example/duantn/activities/TourListActivity.java
@@ -158,12 +158,12 @@ public class TourListActivity extends BaseActivity implements View.OnClickListen
         viewPager2.setOffscreenPageLimit(3);
         viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
         CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
-        compositePageTransformer.addTransformer(new MarginPageTransformer(100));
+        compositePageTransformer.addTransformer(new MarginPageTransformer(50));
         compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
             @Override
             public void transformPage(@NonNull View page, float position) {
                 float r = 1 - Math.abs(position);
-                page.setScaleY(0.85f + r * 0.15f);
+                page.setScaleY(0.80f + r * 0.2f);
 
             }
         });
