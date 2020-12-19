5002a8258e662abe5eab8ddf5dad96675a3539cc
DucAnh <nguyenducanh12042000@gmail.com>
2020-12-19 18:50:40 +0700

    ducanh fix

--- app/src/main/java/com/example/duantn/network/Url.java
+++ app/src/main/java/com/example/duantn/network/Url.java
@@ -2,5 +2,6 @@ package com.example.duantn.network;
 
 public class Url {
     public final static String urlImage = "https://webtourintro.herokuapp.com/";
+//    public final static String urlImage = ":8017/";
     public final static String server = "https://tourintro.herokuapp.com/";
 }
